88f37b45646c20c44a9b19e238d3fe2265321a51
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2016-02-13 18:23:42 +0100

    2016.02.13.4
    Added GMAT data.

--- merge.R
+++ merge.R
@@ -1,22 +1,26 @@
 ### Use this file to merge new data into mega. Beware, the last step overwrites the dataset. If you need to roll back, use Github.
 
 #load from clipboard
-d = read.delim("clipboard")
+d = read.delim("clipboard", stringsAsFactors = F)
 str(d)
 
-#names
-d$abbrev = as_abbrev2(d$Country)
+#subset by missing abbrevs
+d = d[!is.na(d$Names), ]
 
-#subset by missing names
+#names
+d$abbrev = as_abbrev2(d$Names)
+
+#subset by missing abbrevs
 d = d[!is.na(d$abbrev), ]
 
 #rownames
 rownames(d) = d$abbrev
 d$abbrev = NULL
-d$Country = NULL
+d$Names = NULL
 
 #colnames
-colnames(d) = "X0025_" + colnames(d)
+colnames(d) = "X0026_" + colnames(d)
+str(d)
 
 #merge rows
 #useful when an error has been found and two units have been given the same abbreviation
