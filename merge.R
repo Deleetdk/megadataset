1149c915b7407b9411101e9184a94cfe32897bd7
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2016-02-13 16:24:01 +0100

    change versioning
    changed to use date versioning instead of x.y.z.

---
+++ merge.R
@@ -0,0 +1,7 @@
+### Use this file to merge new data into mega. Beware, the last step overwrites the dataset. If you need to roll back, use Github.
+
+
+
+
+# write to mega -----------------------------------------------------------
+write_mega(newmega, filename = "inst/extdata/megadataset.csv")
